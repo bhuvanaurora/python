ee9b8a61f85136b6882e24fa3c25ff1b42afe4e3
bhuvanaurora <bhuvan.aurora@gmail.com>
2014-06-06 18:09:16 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,18 @@
+from os import walk
+
+def get_data():
+	file_names = []
+	for (dirpath, dirnames, filenames) in walk("./files"):
+		file_names.extend(filenames)
+		break
+	return file_names
+
+def main():
+	file_names = get_data()
+	print (file_names)
+	for file_name in file_names:
+		file = open('./files/' + file_name, 'r')
+		print (file.read())
+
+if __name__ == '__main__':
+	main()
